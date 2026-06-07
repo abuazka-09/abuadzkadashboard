75162460b9c5a96195d7d2dfbe5d925845d0d1c7
abuazka-09 <abuazka1510@gmail.com>
2026-06-07 17:17:52 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.ABU_ADZKA_CONFIG = {
-  APPS_SCRIPT_URL: "",
-  ONLINE_MODE: false
+  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbz-zEK8SguAjcCxlPolbz5YwtxhKEBpo83uiFD_6IMRaOys_iD0g3ua4efizmwABB4W/exec",
+  ONLINE_MODE: true
 };
